0519f9dbb40bd200caa94a09249d7fda074c9d02
Luke Novak <contact@nluken.com>
2021-01-26 21:45:07 -0500

    added command line flag handling

--- A2/src/main.go
+++ A2/src/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
-func main(){
+const add = "sum"
+const addDesc = "If toggled, starts the program in sum mode"
+const product = "product"
+const productDesc = "If toggled, starts the program in product mode"
+
+
+func main() {
+	addFlag := flag.Bool(add, false, addDesc)
+	productFlag := flag.Bool(product, false, productDesc)
+
+	flag.Parse()
+
+	err := verifyFlags(addFlag, productFlag)
+	if err != nil {
+		fmt.Printf("Error: %s", err.Error())
+		return
+	}
+
+	// TODO: Full functionality here, calling to a2 package
+	if *addFlag {
+		fmt.Println("now we add")
+	} else {
+		fmt.Println("now we multiply")
+	}
+
 	fmt.Printf("a2 has successfully run")
+}
+
+// ensures that flags are present and not duplicated
+func verifyFlags(addFlag *bool, productFlag *bool) error {
+	if !*addFlag && !*productFlag {
+		return fmt.Errorf("no run mode selected")
+	} else if *addFlag && *productFlag {
+		return fmt.Errorf("multiple run modes selected")
+	} else {
+		return nil
+	}
 }
