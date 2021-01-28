6b71f5e2075aa1f958697c13c5a9ac50c00e2ead
Luke Novak <contact@nluken.com>
2021-01-27 23:38:49 -0500

    finished parsing func

--- A2/src/internal/numJson/parse.go
+++ A2/src/internal/numJson/parse.go
@@ -3,36 +3,52 @@ package numJson
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 )
 
 
 type JInt int
 
 // Decodes into string
-func ParseNumJsonFromStream(d *json.Decoder) (string, error) {
+func ParseNumJsonFromStream(d *json.Decoder) ([]NumJson, error) {
+	var njArray []NumJson
+	var err error
 	for {
 		var r json.RawMessage
-		if err := d.Decode(&r); err != nil {
+		if err = d.Decode(&r); err != nil {
 			println("decode failed")
 			break
 		}
-		var i int
-		var s string
-		var arr []interface{}
-		var obj map[string]interface{}
-		if err := json.Unmarshal(r, &i); err == nil {
-			fmt.Printf("%d\n", i)
-		} else if err = json.Unmarshal(r, &s); err == nil {
-			fmt.Println(s)
-		} else if err = json.Unmarshal(r, &arr); err == nil {
-			fmt.Println("array of shit")
-		} else if err := json.Unmarshal(r, &obj); err != nil {
-			log.Println(err)
-			return "", err
-		} else {
-			println(obj)
-		}
+		njArray = append(njArray, marshalNumJson(r))
 	}
-	return "", nil
+	return njArray, err
+}
+
+// takes a raw json message and marshals it into a NumJson
+func marshalNumJson(r json.RawMessage) NumJson {
+	var i int
+	var s string
+	var arr []json.RawMessage
+	var obj map[string]json.RawMessage
+
+	// NumJson is a union type, so we try to unmarshal it to each subtype
+	if err := json.Unmarshal(r, &i); err == nil {
+		fmt.Printf("%d\n", i)
+		return NumJsonNum(i)
+	} else if err = json.Unmarshal(r, &s); err == nil {
+		fmt.Println(s)
+		return NumJsonString(s)
+	} else if err = json.Unmarshal(r, &arr); err == nil {
+		var njArray []NumJson
+		for _, rawNumJson := range arr {
+			njArray = append(njArray, marshalNumJson(rawNumJson))
+		}
+		return NumJsonArray(njArray)
+	} else if err := json.Unmarshal(r, &obj); err == nil {
+		rawPayload := obj["payload"]
+		njObjMap := make(map[string]NumJson)
+		njObjMap["payload"] = marshalNumJson(rawPayload)
+		return NumJsonObj(njObjMap)
+	} else {
+		panic(obj)
+	}
 }
