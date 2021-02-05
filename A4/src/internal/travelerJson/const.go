4911a136e33e423290c274ab9b68977f48b5df7b
Luke Novak <contact@nluken.com>
2021-02-04 21:30:02 -0500

    added json structs

--- A4/src/internal/travelerJson/const.go
+++ A4/src/internal/travelerJson/const.go
@@ -1,1 +1,33 @@
 package travelerJson
+
+// Top level JSON structures
+type CreateRequest struct {
+	Towns []string `json:"towns"`
+	Roads []FromTo `json:"roads"`
+}
+
+type BatchRequest struct {
+	Characters []CharacterData 	`json:"characters"`
+	Query QueryData				`json:"query"`
+}
+
+type ResponseData struct {
+	Invalid []CharacterData	`json:"invalid"`
+	Response bool			`json:"response"`
+}
+
+// Data Object types DOTs
+type FromTo struct {
+	From string `json:"from"`
+	To string	`json:"to"`
+}
+
+type CharacterData struct {
+	Town string	`json:"town"`
+	Name string	`json:"name"`
+}
+
+type QueryData struct {
+	Character string 	`json:"character"`
+	Destination string 	`json:"destination"`
+}
