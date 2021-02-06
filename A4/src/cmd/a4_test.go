bd751816075b50a033688daa7f806ea0dd54d9dc
Luke Novak <contact@nluken.com>
2021-02-05 19:15:28 -0500

    refactored connection test

--- A4/src/cmd/a4_test.go
+++ A4/src/cmd/a4_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"net"
 	"testing"
 )
@@ -13,25 +12,31 @@ const (
 // tests that the startup process works fine
 func TestStartup(t *testing.T) {
 
-	go func() {
-		listener, err := net.Listen("tcp", ":8080")
-		if err != nil {
-			t.Fatal(err)
-		}
-		conn, _ := listener.Accept()
-		defer conn.Close()
-		var name bytes.Buffer
-		_, err = conn.Read(name.Bytes())
-		if err != nil {
-			t.Fatal(err)
-		}
-		_, err = conn.Write([]byte(idMessage))
-		if err != nil {
-			t.Fatal(err)
-		}
+	// start a mock server
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	}()
+	// this does not return anything, and we can't capture stdout. dunno how we verify the correct message is being
+	// written to the terminal
+	go connectToServer("", defaultPort, defaultName)
 
-	connectToServer("", defaultPort, defaultName)
+	conn, _ := listener.Accept()
+	defer conn.Close()
+	nameBuf := make([]byte, 4096)
+	l, err := conn.Read(nameBuf)
+	nameBuf = nameBuf[0:l]
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(nameBuf) != defaultName {
+		t.Fail()
+	}
+	_, err = conn.Write([]byte(idMessage))
+	if err != nil {
+		t.Fatal(err)
+	}
+	return
 
 }
