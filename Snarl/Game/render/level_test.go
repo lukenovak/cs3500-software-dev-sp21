555cc53e0ac0f9ee8c2540ce0ae1e647ec52fee7
Luke Novak <contact@nluken.com>
2021-02-16 16:54:39 -0500

    added a third rendering test

--- Snarl/Game/render/level_test.go
+++ Snarl/Game/render/level_test.go
@@ -5,9 +5,14 @@ import (
 	"testing"
 )
 
-func TestRenderRoom(t *testing.T) {
-	testLevel := level.NewEmptyLevel(3, 3)
-	err := testLevel.GenerateRectangularRoom(level.NewPosition2D(0, 0), 3, 3, nil)
+func TestRenderLevelSingleRoom(t *testing.T) {
+
+	// test a normal level
+	testLevel, err := level.NewEmptyLevel(3, 3)
+	if &testLevel == nil {
+		t.Fatal("unable to generate empty level")
+	}
+	err = testLevel.GenerateRectangularRoom(level.NewPosition2D(0, 0), 3, 3, nil)
 	if err != nil {
 		t.Fatal("unable to generate level")
 	}
@@ -22,14 +27,47 @@ func TestRenderRoom(t *testing.T) {
 	} else {
 		print(render)
 	}
+
+	// test a large level
+	testLevel, err = level.NewEmptyLevel(9, 6)
+	if &testLevel == nil {
+		t.Fatal("unable to generate empty level")
+	}
+	err = testLevel.GenerateRectangularRoom(level.NewPosition2D(0, 0), 9, 6, nil)
+	if err != nil {
+		t.Fatal("unable to generate level")
+	}
+	render = RenderLevel(testLevel)
+	secondTestRender :=
+			"▓▓▓▓▓▓▓▓▓\n" +
+			"▓░░░░░░░▓\n" +
+			"▓░░░░░░░▓\n" +
+			"▓░░░░░░░▓\n" +
+			"▓░░░░░░░▓\n" +
+			"▓▓▓▓▓▓▓▓▓\n"
+
+	if render != secondTestRender {
+		t.Fail()
+	} else {
+		print(render)
+	}
 }
 
-func TestRenderLevel(t *testing.T) {
-	genLevel := level.NewEmptyLevel(8, 8)
+func TestRenderFullLevel(t *testing.T) {
+	genLevel, err := level.NewEmptyLevel(8, 8)
+	if &genLevel == nil {
+		t.Fatal("unable to generate empty level")
+	}
 	firstRoomDoor, secondRoomDoor := []level.Position2D{level.NewPosition2D(3,2)}, []level.Position2D{level.NewPosition2D(5,4)}
-	genLevel.GenerateRectangularRoom(level.NewPosition2D(0,0), 4, 4, firstRoomDoor)
-	genLevel.GenerateRectangularRoom(level.NewPosition2D(4, 4), 4, 4, secondRoomDoor)
-	err := genLevel.GenerateHallway(firstRoomDoor[0], secondRoomDoor[0], []level.Position2D{level.NewPosition2D(5, 2)})
+	err = genLevel.GenerateRectangularRoom(level.NewPosition2D(0,0), 4, 4, firstRoomDoor)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = genLevel.GenerateRectangularRoom(level.NewPosition2D(4, 4), 4, 4, secondRoomDoor)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = genLevel.GenerateHallway(firstRoomDoor[0], secondRoomDoor[0], []level.Position2D{level.NewPosition2D(5, 2)})
 	if err != nil {
 		t.Fatal(err)
 	}
