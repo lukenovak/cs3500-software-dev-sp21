582e3f2742f99d9c15bab819c3f14f47fff0fbab
Luke Novak <contact@nluken.com>
2021-02-16 20:01:56 -0500

    created temporary main file to demonstrate level rendering

--- Snarl/Game/cmd/go.mod
+++ Snarl/Game/cmd/go.mod
@@ -1,13 +1,16 @@
-module "github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/Game/render"
+module "github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/Game/cmd"
 
 go 1.15
 
 require (
 	"github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/Game/level" v0.0.0
+	"github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/Game/render" v0.0.0
 	"github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/Game/item" v0.0.0
+
 )
+
 replace (
 	"github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/Game/level" v0.0.0 => ../level
+	"github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/Game/render" v0.0.0 => ../render
 	"github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/Game/item" v0.0.0 => ../item
-
 )
--- Snarl/Game/cmd/main.go
+++ Snarl/Game/cmd/main.go
@@ -1,5 +1,67 @@
 package main
 
+import (
+	"github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/Game/level"
+	"github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/Game/render"
+)
+
 func main() {
-	println("Welcome to snarl!")
+	println("Welcome to snarl ascii level renderer!")
+	println("Level 1: Simple 3 x 3 room:")
+	firstLevel := generateFirstLevel()
+	print(render.ASCIILevel(firstLevel))
+
+	println("Level 2: Larger room:")
+	secondLevel := generateSecondLevel()
+	print(render.ASCIILevel(secondLevel))
+
+	println("Level 3: Two rooms with hallways")
+	thirdLevel := generateThirdLevel()
+	print(render.ASCIILevel(thirdLevel))
+}
+
+func generateFirstLevel() level.Level {
+	testLevel, err := level.NewEmptyLevel(3, 3)
+	if &testLevel == nil {
+		panic("unable to generate empty level")
+	}
+	err = testLevel.GenerateRectangularRoom(level.NewPosition2D(0, 0), 3, 3, nil)
+	if err != nil {
+		panic("unable to generate level")
+	}
+	return testLevel
+}
+
+func generateSecondLevel() level.Level {
+	// test a large level
+	testLevel, err := level.NewEmptyLevel(9, 6)
+	if &testLevel == nil {
+		panic("unable to generate empty level")
+	}
+	err = testLevel.GenerateRectangularRoom(level.NewPosition2D(0, 0), 9, 6, nil)
+	if err != nil {
+		panic("unable to generate level")
+	}
+	return testLevel
+}
+
+func generateThirdLevel() level.Level {
+	genLevel, err := level.NewEmptyLevel(8, 8)
+	if &genLevel == nil {
+		panic(err)
+	}
+	firstRoomDoor, secondRoomDoor := []level.Position2D{level.NewPosition2D(3,2)}, []level.Position2D{level.NewPosition2D(5,4)}
+	err = genLevel.GenerateRectangularRoom(level.NewPosition2D(0,0), 4, 4, firstRoomDoor)
+	if err != nil {
+		panic(err)
+	}
+	err = genLevel.GenerateRectangularRoom(level.NewPosition2D(4, 4), 4, 4, secondRoomDoor)
+	if err != nil {
+		panic(err)
+	}
+	err = genLevel.GenerateHallway(firstRoomDoor[0], secondRoomDoor[0], []level.Position2D{level.NewPosition2D(5, 2)})
+	if err != nil {
+		panic(err)
+	}
+	return genLevel
 }
