22cbd6749de4e6ac1c37c85725e078c71b92ba0e
--global <victoria.w@northeastern.edu>
2021-03-03 06:53:54 -0500

    init testing harness

--- Snarl/tests/Level/cmd/levelTest/main.go
+++ Snarl/tests/Level/cmd/levelTest/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	testJson "github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/tests/Level/json"
 	"github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/tests/Level/tester"
 	"os"
 )
 
-func main()  {
-	output := tester.TestLevelTileData(testJson.ParseLevelTileDataTestJson(os.Stdin))
-	println(output)
+func main() {
+	outputMsg := tester.TestLevelTileData(testJson.ParseLevelTileDataTestJson(os.Stdin))
+	outputJsonBytes, err := json.Marshal(outputMsg)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(string(outputJsonBytes))
 }
--- Snarl/tests/Level/tester/levelTileData.go
+++ Snarl/tests/Level/tester/levelTileData.go
@@ -1,8 +1,33 @@
 package tester
 
-import "github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/tests/Level/json"
+import (
+	"github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/src/Game/level"
+	"github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/tests/Level/json"
+)
 
 // Given a point, gives information about that particular tile
-func TestLevelTileData(testLevel json.LevelTestLevel) string {
-	panic("Not yet implemented")
+func TestLevelTileData(testLevel json.LevelTestLevelInput) string {
+	newLevel, err := level.NewEmptyLevel(0, 0)
+	if err != nil {
+		panic("unable to generate new empty level")
+	}
+
+	// generate rooms
+	for _, room := range testLevel.Level.Rooms {
+		newOrigin := room.Origin.To2DPosition()
+		newLevel.GenerateRectangularRoomWithLayout(newOrigin, room.Bounds[0], room.Bounds[1], room.Layout)
+	}
+
+	// generate hallways
+	for _, hallway := range testLevel.Level.Hallways {
+		newFrom := hallway.From.To2DPosition()
+		newTo := hallway.To.To2DPosition()
+		var newWaypoints []level.Position2D
+		for _, point := range hallway.Waypoints {
+			newWaypoints = append(newWaypoints, point.To2DPosition())
+		}
+		newLevel.GenerateHallway(newFrom, newTo, newWaypoints)
+	}
+
+	return newLevel.GetTileData(testLevel.Point.To2DPosition())
 }
