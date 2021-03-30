7bfc62cdf3ca93cb2ae06501673a5025a62454f1
Luke Novak <contact@nluken.com>
2021-03-30 00:25:44 -0400

    basic zombie move calculations

--- Snarl/src/Game/state/adversaryClient.go
+++ Snarl/src/Game/state/adversaryClient.go
@@ -5,7 +5,7 @@ import "github.ccs.neu.edu/CS4500-S21/Ormegland/Snarl/src/Game/level"
 // client controller that determines an Adversary's moves
 type AdversaryClient interface {
 	// Returns a Response with the best relative move
-	CalculateMove(playerPosns []level.Position2D) Response
+	CalculateMove(playerPosns []level.Position2D, adversaryPositions []level.Position2D) Response
 }
 
 type ExampleAdvClient struct {
--- Snarl/src/Game/state/localAdversary.go
+++ Snarl/src/Game/state/localAdversary.go
@@ -7,9 +7,12 @@ import (
 // client that moves ghost-type enemies
 type GhostClient struct {
 	Name string
+	LevelData level.Level
+	MoveDistance int
+	CurrentPosn level.Position2D
 }
 
-func (g GhostClient) CalculateMove(playerPosns []level.Position2D) Response {
+func (g GhostClient) CalculateMove(playerPosns []level.Position2D, adversaryPosns []level.Position2D) Response {
 	return Response{
 		PlayerName: g.Name,
 		Move:       level.Position2D{0, 0},
@@ -24,10 +27,21 @@ type ZombieClient struct {
 	CurrentPosn level.Position2D
 }
 
-func (z ZombieClient) CalculateMove(playerPosns []level.Position2D) Response {
+func (z ZombieClient) CalculateMove(playerPosns []level.Position2D, adversaryPosns []level.Position2D) Response {
+	var move level.Position2D
+	for _, posn := range playerPosns {
+		validMoveTiles := z.LevelData.GetWalkableTilePositions(z.CurrentPosn, z.MoveDistance)
+		minDistance := z.LevelData.Size.Col * z.LevelData.Size.Row
+		for _, pos := range validMoveTiles {
+			if pos.GetManhattanDistance(posn) < minDistance {
+				// TODO: MAKE THIS RELATIVE
+				move = pos
+			}
+		}
+	}
 	return Response{
 		PlayerName: z.Name,
-		Move:       level.Position2D{0, 0},
+		Move:       move,
 	}
 }
 
